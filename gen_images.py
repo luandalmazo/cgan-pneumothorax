5bfbcc32f71e6fa92f784f1b1a80c98ccb168867
Thomas Todt <thomas.bianchi.todt@gmail.com>
2023-11-03 12:12:03 -0300

    new update

---
+++ gen_images.py
@@ -0,0 +1,16 @@
+import torch
+import utils
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+model = torch.load("gen-2023-11-03\ 12:02:23.539974.pkl", map_location=device)
+if not torch.cuda.is_available():
+    model = model.module
+model.eval()
+model = model.to(device)
+
+labels = torch.tensor((0, 1))
+
+fake_images = model(labels)
+
+utils.show_tensor_images(fake_images, show="plot")
